7b14b77a6e8ca51082bf48dcd32ad11a6fa8d1f2
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-10-01 11:35:12 +0500

    Update App.js

--- tailor-frontend/src/App.js
+++ tailor-frontend/src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>welcome to deployment site</h1>
-      </header>
+     <h1>welcome</h1>
     </div>
   );
 }
