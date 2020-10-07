d90a87fe77e9602e46a73a1b02df4c5d1671661b
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-10-07 20:02:57 +0500

    make a backend and update login and sign up

--- tailor-frontend/src/components/LogIn.js
+++ tailor-frontend/src/components/LogIn.js
@@ -2,12 +2,30 @@ import React from 'react';
 
 
 
-const LogIn = ()=>{
-    return(
-        <div>
-    
-     
-</div>   )
+const LogIn = () => {
+  return (
+    <div>
+       <form >
+
+<div className="form-control">
+  <label htmlFor="name">UserName</label>
+  <input type="text" id="username-register"placeholder="Enter User name or Email..."  required/>
+</div>
+
+
+<div className="form-control">
+  <label htmlfor="password">Password</label>
+  <input type="password"  id="password-register" placeholder="Enter Password..." required/>
+</div>
+
+
+
+
+<button className="btn">Log IN</button>
+
+</form>
+    </div>
+    )
 }
 
 export default LogIn
--- tailor-frontend/src/components/SignUp.js
+++ tailor-frontend/src/components/SignUp.js
@@ -2,15 +2,37 @@ import React from 'react';
 
 
 
-const SignUp = ()=>{
-    return(
-        <div>
-          
-  
-   
-      
+const SignUp = () => {
+  return (
+    <div>
+      <h2>Create an Account</h2>
+      <form >
+
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="username-register" placeholder="Enter Name..."  required/>
         </div>
-    )
+
+
+        <div className="form-control">
+          <label htmlfor="password">Password</label>
+          <input type="password" id="password-register" placeholder="Enter Password..." required/>
+        </div>
+
+        <div className="form-control">
+          <label htmlfor="Email">Email</label>
+          <input type="Email"  id="email  -register" placeholder="Enter Email..." required/>
+        </div>
+
+
+        <button className="btn">Sign Up</button>
+
+      </form>
+
+
+
+    </div>
+  )
 }
 
 export default SignUp
