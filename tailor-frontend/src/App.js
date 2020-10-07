261d537988d9b0b5d730607f076c39b1a37b075b
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-10-07 15:44:11 +0500

    update app.css and remove login and sign up

--- tailor-frontend/src/App.js
+++ tailor-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/header';
 import Home from './components/Home'
@@ -10,13 +10,14 @@ import SignUp from './components/SignUp'
 function App() {
   return (
     <BrowserRouter>
-    <div className="container">
-     <Header/>
-     <Route path="/" exact component={Home}></Route>
-     <Route path="/login" exact component={LogIn}></Route>
-     <Route path="/signup" exact component={SignUp}></Route>
-
-    </div>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/login" exact component={LogIn}></Route>
+          <Route path="/signup" exact component={SignUp}></Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
