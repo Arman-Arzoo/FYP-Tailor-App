4945b88890ac2c69b996b79db4ae0b5fad700e9e
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-10-09 22:47:05 +0500

    Update userRoute.js

--- tailor-backend/routes/userRoute.js
+++ tailor-backend/routes/userRoute.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 
 
-router.get('/test', (req,res)=>{
-    res.send ("hello it working");
-})
+router.post("/register", async (req, res) => {
 
+    const { email, password, passwordCheck, displayName } = req.body;
+
+    if (!email || !password || !passwordCheck) {
+
+        return res.status(400).json({msg:"not all feild have been entered"});
+
+    }
+
+});
 module.exports = router
 
 
