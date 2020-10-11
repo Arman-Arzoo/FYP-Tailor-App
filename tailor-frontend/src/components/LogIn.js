ec2b6a4acde1d97d315af99abe086fa3f6c03233
Arman-Arzoo <51259303+Arman-Arzoo@users.noreply.github.com>
2020-10-11 20:47:09 +0500

    update app.css ,login.js ,signup.js and make a file errorNotices.js
    error identification

--- tailor-frontend/src/components/LogIn.js
+++ tailor-frontend/src/components/LogIn.js
@@ -3,6 +3,7 @@ import UserContext from '../context/userContext';
 import {useState,useContext} from 'react';
 import Axios from 'axios';
 import {useHistory} from 'react-router-dom';
+import ErrorNotice from './misc/errorNotices';
 
 
 
@@ -10,12 +11,15 @@ const LogIn = () => {
 
   const [email ,setEmail] = useState();
   const [password ,setPassword] = useState();
+  const [error, setError] = useState();
 
   const {setUserData} = useContext(UserContext);
   const history = useHistory();
 
   const submit =async(e)=>{
     e.preventDefault();
+
+    try{
     const logInUser = {email,password};
     const loginRes = await Axios.post("http://localhost:4000/users/login",logInUser);
 
@@ -26,24 +30,32 @@ const LogIn = () => {
     });
     localStorage.setItem("auth-token",loginRes.data.token);
     history.push("/")
+  
+  }
+  catch(err){
+    err.response.data.msg && setError(err.response.data.msg);
 
-
+  }
   }
 
 
   return (
     <div>
+      <h2>Log in</h2>
+      {error && <ErrorNotice message={error}  clearError ={()=>{setError(undefined)}}/>}
        <form onSubmit={submit}>
+
+
 
 <div className="form-control">
   <label htmlFor="name">UserName</label>
-  <input type="text" id="username-login"placeholder="Enter User name or Email..." onChange={(e)=>setEmail(e.target.value)} required/>
+  <input type="text" id="username-login"placeholder="Enter User name or Email..." onChange={(e)=>setEmail(e.target.value)} />
 </div>
 
 
 <div className="form-control">
   <label htmlfor="password">Password</label>
-  <input type="password"  id="password-login" placeholder="Enter Password..." onChange={(e)=>setPassword(e.target.value)} required/>
+  <input type="password"  id="password-login" placeholder="Enter Password..." onChange={(e)=>setPassword(e.target.value)} />
 </div>
 
 
--- tailor-frontend/src/components/SignUp.js
+++ tailor-frontend/src/components/SignUp.js
@@ -3,6 +3,7 @@ import UserContext from '../context/userContext';
 import {useState,useContext} from 'react';
 import Axios from 'axios';
 import {useHistory} from 'react-router-dom';
+import ErrorNotice from './misc/errorNotices';
 
 
 
@@ -13,6 +14,7 @@ const SignUp = () => {
   const [password ,setPassword] = useState();
   const [passwordCheck ,setPasswordCheck] = useState();
   const [displayName,setDisplayName] = useState();
+  const [error, setError] = useState();
 
 
   const {setUserData} = useContext(UserContext);
@@ -20,6 +22,7 @@ const SignUp = () => {
 
   const submit =async(e)=>{
     e.preventDefault();
+    try{
     const newUser = {email,password,passwordCheck,displayName};
     await Axios.post("http://localhost:4000/users/register",newUser);
 
@@ -33,33 +36,41 @@ const SignUp = () => {
     });
     localStorage.setItem("auth-token",loginRes.data.token);
     history.push("/")
+  }catch(err){
+
+    err.response.data.msg && setError(err.response.data.msg);
+
+
+  }
 
 
   }
   return (
     <div>
       <h2>Create an Account</h2>
+
+      {error && <ErrorNotice message={error}  clearError ={()=>{setError(undefined)}}/>}
       <form onSubmit={submit} >
 
         <div className="form-control">
           <label htmlFor="username-register">Email</label>
-          <input type="email" id="username-register" placeholder="Enter Name..." onChange={(e)=>setEmail(e.target.value)} required/>
+          <input type="email" id="username-register" placeholder="Enter Name..." onChange={(e)=>setEmail(e.target.value)} />
         </div>
 
 
         <div className="form-control">
           <label htmlfor="password-register">Password</label>
-          <input type="password" id="password-register" placeholder="Enter Password..." onChange={(e)=>setPassword(e.target.value)} required/>
+          <input type="password" id="password-register" placeholder="Enter Password..." onChange={(e)=>setPassword(e.target.value)} />
           <br/>
           <label htmlfor="password-register">RePassword</label>
-          <input type="password" id="password-check" placeholder="verify password..."onChange={(e)=>setPasswordCheck(e.target.value)} required/>
+          <input type="password" id="password-check" placeholder="verify password..."onChange={(e)=>setPasswordCheck(e.target.value)} />
         </div>
 
       
 
         <div className="form-control">
           <label htmlfor="displayName-register">Display Name</label>
-          <input type="text"  id="email-register" placeholder="Enter Display Name..." onChange={(e)=>setDisplayName(e.target.value)} required/>
+          <input type="text"  id="email-register" placeholder="Enter Display Name..." onChange={(e)=>setDisplayName(e.target.value)} />
         </div>
 
 
